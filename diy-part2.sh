0ece724582d37532592f1a956d7997d87dc9665c
名侦探柯北 <857112551@qq.com>
2023-04-25 11:38:53 +0800

    Add files via upload
    首次提交

---
+++ diy-part2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+#rm -rf ./package/diy-ziyong/theme
+#rm -rf ./package/diy-ziyong/luci-app-wrtbwmon-zh
+#rm -rf ./package/diy-ziyong/wrtbwmon
+#rm -rf ./feeds/packages/net/adguardhome
+#rm -rf ./feeds/packages/net/smartdns
+#rm -rf ./feeds/packages/net/mosdns
+#rm -rf ./feeds/luci/applications/luci-app-passwall
+#rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+#rm -rf ./feeds/luci/applications/luci-app-openclash
+#rm -rf ./feeds/luci/applications/luci-app-smartdns
